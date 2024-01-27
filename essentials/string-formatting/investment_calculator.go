ffc5db811d80be231936ae0a282539d90fdbb47f
Vladimir Stoyanov <vladsto101@gmail.com>
2024-01-27 18:22:19 +0200

    Added first exercise and string formatting.

---
+++ essentials/string-formatting/investment_calculator.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/1000, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	formattedFV := fmt.Sprintf("Future Real Value: %.2f", futureRealValue)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+
+	fmt.Printf("Future Real Value: %v", futureRealValue)
+	fmt.Println()
+	fmt.Printf("Future Real Value: %.2f", futureRealValue)
+	fmt.Println()
+	fmt.Println(formattedFV)
+
+	fmt.Printf(`Future Real Value: %.2f
+	`, futureRealValue)
+}
