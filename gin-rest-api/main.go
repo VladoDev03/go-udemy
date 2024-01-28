9804eab04973ae30f3493b1b8ed4a52f9dbd45d3
Vladimir Stoyanov <vladsto101@gmail.com>
2024-01-28 19:53:27 +0200

    Added get event by id.

--- gin-rest-api/main.go
+++ gin-rest-api/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"rest-api/db"
 	"rest-api/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,6 +15,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
@@ -29,6 +31,24 @@ func getEvents(context *gin.Context) {
 
 	// context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id."})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not fetch event."})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
--- gin-rest-api/models/event.go
+++ gin-rest-api/models/event.go
@@ -56,7 +56,7 @@ func GetAllEvents() ([]Event, error) {
 	for rows.Next() {
 		var event Event
 
-		rows.Scan(
+		err = rows.Scan(
 			&event.ID,
 			&event.Name,
 			&event.Description,
@@ -73,4 +73,26 @@ func GetAllEvents() ([]Event, error) {
 	}
 
 	return events, nil
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+
+	err := row.Scan(
+		&event.ID,
+		&event.Name,
+		&event.Description,
+		&event.Location,
+		&event.DateTime,
+		&event.UserID,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
 }
